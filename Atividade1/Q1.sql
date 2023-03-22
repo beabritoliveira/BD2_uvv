185ad6ab76c33cb80822ef7405b90ccae0193cc8
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-03-22 16:07:56 -0300

    Update Q1.sql

--- Atividade1/Q1.sql
+++ Atividade1/Q1.sql
@@ -1,3 +1,7 @@
+/* Escreva uma consulta que retorne o nome e sobrenome de todos os administradores (officer) com o nome
+da empresa que eles administram (business.name) e cidade onde ela está presente (customer.city).
+*/
+
 SELECT CONCAT((officer.lname), " ,", (officer.fname)) as administrador, business.name, customer.city
 from business 
 INNER JOIN officer ON (business.cust_id = officer.cust_id)
