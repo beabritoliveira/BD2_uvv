8ff6980314df0c18788d25508fa6e1354c341ef8
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-03-25 15:21:56 -0300

    Update Q5.sql

--- Atividade1/Q5.sql
+++ Atividade1/Q5.sql
@@ -1,3 +1,4 @@
+/*Create view na questão 2*/
 Create OR REPLACE VIEW view_cliente AS (
 	SELECT DISTINCT i.cust_id, CONCAT((i.fname), " ,", (i.lname)) as nomeCliente
     From customer c
@@ -10,3 +11,31 @@ Create OR REPLACE VIEW view_cliente AS (
 );
 SELECT * 
 from view_cliente;
+
+/*Create view na questão 4*/
+CREATE or REPLACE view view_quatro AS
+  SELECT contas, nomeTitular, nomeAgencia, valor
+  FROM(
+	SELECT a.account_id as contas, 
+    		CONCAT((i.fname), " ", (i.lname)) as nomeTitular, 
+    		b.name as NomeAgencia,
+    		pending_balance
+  	FROM  account a
+    INNER Join individual i ON (a.cust_id = i.cust_id)
+    inner JOIN branch b on (a.open_branch_id = b.branch_id)
+  	UNION
+  	SELECT a.account_id as contas, 
+    		CONCAT((o.fname), " ", (o.lname)) as nomeTitular, 
+    		b.name as NomeAgencia,
+    		pending_balance
+  	FROM account a
+    INNER JOIN officer o on (a.cust_id = o.cust_id)
+    INNER JOIN branch b ON (a.open_branch_id = b.branch_id)
+    ) as tbv, (
+      SELECT MAX(pending_balance) as valor
+      from account
+      GROUP By open_branch_id
+    ) AS saldo
+  Where saldo.valor = tbv.pending_balance ;
+
+SELECT * from view_quatro;
