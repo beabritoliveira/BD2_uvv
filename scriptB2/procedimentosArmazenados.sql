5e8f29f0aeece75eb6d95a2af66cd20df382b383
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-06-07 20:55:54 -0300

    Update procedimentosArmazenados.sql

--- scriptB2/procedimentosArmazenados.sql
+++ scriptB2/procedimentosArmazenados.sql
@@ -75,3 +75,78 @@ DELIMITER ;
 CALL povoar_funcionario(100);
  
 select * from funcionario;
+
+
+CREATE table licenca_sanitaria(
+	num_licenca char(3) not null,
+  	data_emissao date not null,
+  	validade date not null,
+  	cnpj char(14) not null,
+	CONSTRAINT pk_LS PRIMARY KEY (num_licenca)
+);
+
+CREATE table conexao(
+		ordem int not null,
+        num_lic char(3) not null
+    );
+    
+DELIMITER $$   
+CREATE TRIGGER testando AFTER INSERT ON licenca_sanitaria FOR EACH ROW 
+	BEGIN
+		INSERT INTO conexao (ordem, num_lic)
+		VALUES((select COUNT(num_licenca) from licenca_sanitaria)), (CAST(lic AS CHAR));
+	END $$
+
+CREATE PROCEDURE criando_alvaraSanitaria(in licenca int)
+BEGIN
+	DECLARE lic int;
+	DECLARE id int;
+    DECLARE num int;
+    
+	WHILE licenca >= 1 DO
+		SET lic = RAND() * 1000; /*Sorteia a licenca*/
+		SET id = RAND() * 100000000000000;  /*Sorteia o CNPJ*/
+        SET num = (select COUNT(num_lic) from conexao);
+        
+        WHILE licenca > (licenca + num) && licenca != 0 DO
+			IF lic <=> (select num_lic from conexao where ordem = licenca) THEN
+				SET lic = RAND() * 1000;
+			END IF;
+            
+            SET num = num - 1;
+		END WHILE;
+        
+		INSERT INTO licenca_sanitaria (num_licenca, data_emissao, validade, cnpj)
+		VALUES ((CAST(lic AS CHAR)), 191203, 201203, (CAST(id AS CHAR)));
+		
+		SET licenca = licenca - 1;    
+	END WHILE;
+END $$
+DELIMITER ;
+CALL criando_alvaraSanitaria(200);
+
+select * from licenca_sanitaria;
+
+
+
+DELIMITER $$
+CREATE PROCEDURE povoar_Restaurante(in qnt_restaurante int)
+BEGIN
+	DECLARE num;
+    DECLARE total_funcionario int;
+    SET num = 1;
+    SET total_funcionario = (SELECT COUNT(id_funcionario) FROM funcionario);
+    
+    WHILE total_funcionario > 1 DO
+		IF total_funcionario <=> ( SELECT id_funcionario FROM funcionario WHERE funcao = chefe)
+		
+		INSERT INTO restaurante (nome, id_chefe, horario_abertura, horario_fechar,
+						cnpj_responsavel, alvara_sanitario, categoria, id_fed)
+		VALUES(,total_funcionario, null, null,
+				(SELECT cnpj FROM licenca_sanitaria WHERE  )
+                );
+		
+    END WHILE;
+END $$
+DELIMITER ;
+CALL povoar_Restaurante(200);
