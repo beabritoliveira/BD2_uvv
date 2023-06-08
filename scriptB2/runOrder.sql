b6b0c9245284c93cdbca14874cbd9066ec8d7c14
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-06-08 19:15:42 -0300

    Create runOrder.sql

---
+++ scriptB2/runOrder.sql
@@ -0,0 +1,92 @@
+/*  CRIANDO TABELAS  */
+CREATE table restaurante(
+	id_restaurante int not null AUTO_INCREMENT,
+  	nome varchar(50) not null, 
+  	id_chefe int , /*FK que referencia a tabela funcionario*/
+  	horario_abertura time,
+  	horario_fechar time,
+  	cnpj_responsavel char(14) not null,
+  	alvara_sanitario char(3) not null, /*FK que referencia a tabela licença sanitaria*/
+  	categoria varchar(30),
+  	id_fed int, /*FK que referencia a tabela federação*/
+  	CONSTRAINT pk_restaurante PRIMARY KEY (id_restaurante),
+  	UNIQUE KEY restaurante_nome_unique_idx (nome)
+);
+
+CREATE table funcionario(
+	id_funcionario int NOT null AUTO_INCREMENT,
+  	sobrenome varchar(25),
+  	nome varchar(25),
+  	sexo char(1),
+  	data_contratacao date not null,
+  	funcao varchar(35) not null, 
+  	telefone char(11),
+  	CONSTRAINT pk_funcionario PRIMARY KEY (id_funcionario)
+);
+
+ALTER TABLE funcionario ADD CONSTRAINT ck_func_sexo 
+CHECK (sexo IN('M', 'F'));
+
+CREATE TABLE trabalha_em(
+  	id_funcionario int NOT null,
+  	id_restaurante int not null,
+    CONSTRAINT pk_trabalha_em PRIMARY KEY (id_funcionario, id_restaurante)
+  );
+
+CREATE table fornecedor(
+	id_fornecedor int not null AUTO_INCREMENT,
+  	id_restaurante int, /*FK que referencia a tabela restaurante*/
+  	item_fornecido int not null, /*FK que referencia a tabela item cardápio*/
+  	telefone char(11),
+  	CONSTRAINT pk_fornecedor PRIMARY KEY (id_fornecedor)
+);
+
+CREATE table federacao(
+	id_fed int not null AUTO_INCREMENT,
+  	cidade varchar(45) not null,
+ 	estado char(2) not null,
+ 	endereco varchar(100),
+   	CONSTRAINT pk_federacao PRIMARY KEY (id_fed)   
+);
+
+CREATE TABLE cardapio(
+	id_restaurante int not null,
+    id_cardapio int not null AUTO_INCREMENT,
+    CONSTRAINT pk_cardapio PRIMARY KEY(id_cardapio, id_restaurante)
+);
+
+CREATE table itemCardapio(
+	id_item int not null AUTO_INCREMENT,
+  	id_cardapio int not null, /*FK que referencia a tabela cardapio*/ 
+  	valor decimal(5,2),
+  	minutos_preparo int,
+  	nome_item varchar(60) null,
+  	CONSTRAINT pk_itemCardapio PRIMARY KEY (id_item)
+);
+
+ALTER TABLE itemCardapio ADD CONSTRAINT ck_itemCardapio_minutos_preparo 
+CHECK (minutos_preparo >= 0);
+
+CREATE table licenca_sanitaria(
+	num_licenca char(3) not null,
+  	data_emissao date not null,
+  	validade date not null,
+  	cnpj char(14) not null,
+	CONSTRAINT pk_LS PRIMARY KEY (num_licenca)
+);
+
+/*  TRIGGERS  */
+DELIMITER $$   
+CREATE TRIGGER testando AFTER INSERT ON licenca_sanitaria FOR EACH ROW 
+	BEGIN
+		INSERT INTO conexao (ordem, num_lic)
+		VALUES((select COUNT(num_licenca) from licenca_sanitaria), new.num_licenca);
+        /*(CAST(lic AS CHAR));*/
+END $$
+
+DELIMITER $$   
+CREATE TRIGGER povoar_trabalha_em_CHEF AFTER INSERT ON restaurante FOR EACH ROW 
+	BEGIN
+    INSERT Into trabalha_em (id_restaurante, id_funcionario)
+	VALUES (new.id_restaurante, new.id_chefe);
+END $$
