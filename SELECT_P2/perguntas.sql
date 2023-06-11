37dc9e887307c4fd9f214e3d462056f7e2b55778
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2023-06-11 14:16:01 -0300

    Update perguntas.sql

--- SELECT_P2/perguntas.sql
+++ SELECT_P2/perguntas.sql
@@ -1,15 +1,3 @@
-
-*/ESTAO 1
-Escreva uma consulta que retorne por Chef o nome do restaurante que ele trabalha,
-a cidade que ele está localizado e a, o nome do chef /*
-    SELECT id_chefe, r.nome, f.nome, cidade
-    FROM restaurante r
-    INNER JOIN funcionario f ON (r.id_chefe = f.id_funcionario)
-    INNER JOIN federacao fed ON (r.id_fed = fed.id_fed)
-    ORDER BY id_chefe;
-
-
-
 /*QUESTAO 1
 Escreva uma consulta que retorne por Chef o nome do restaurante que 
 ele trabalha,
@@ -37,12 +25,7 @@ GROUP BY cnpj;
 Escreva uma consulta que identifique o restaurante que mais possui funcionarios e retorne 
 o nome e o id de todos os que trabalham lá
 */
-	/*SELECT id_restaurante, f.id_funcionario, CONCAT(nome, '  ' , sobrenome) as nome, funcao
-    FROM trabalha_em te
-    INNER JOIN funcionario f USING (id_funcionario)
-    WHERE id_restaurante = 9;*/
-    
-    
+
     SELECT t.id_restaurante, f.id_funcionario, CONCAT(nome, '  ' , sobrenome) as nome, funcao
     FROM
     (SELECT id_restaurante, COUNT(id_funcionario) as quant_funci
